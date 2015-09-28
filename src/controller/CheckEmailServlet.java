9698ffd8870f4c7eb9bb732ae300ca474287f4f8
somedayiws <somedayiws@gmail.com>
2015-09-28 09:21:54 +0700

    xx

---
+++ src/controller/CheckEmailServlet.java
@@ -0,0 +1,36 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.bean.TAIKHOAN;
+import model.bo.ThongBaoBO;
+
+@WebServlet("/CheckEmailServlet")
+public class CheckEmailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public CheckEmailServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		ThongBaoBO tb = new ThongBaoBO();
+		TAIKHOAN user = (TAIKHOAN)request.getSession().getAttribute("user");
+		request.setAttribute("soEmail", tb.CheckMail(user.getIdTaiKhoan(), user.getTenTaiKhoan()));
+		request.getRequestDispatcher("CheckEmail.jsp").forward(request, response);
+	}
+
+}
